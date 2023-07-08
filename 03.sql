559ba7f81f5892d6a4388aee16ed3c9ef85ed77a
Agnieszka Thiel <115164339+AgInsideOut@users.noreply.github.com>
2023-07-08 14:27:19 +0200

    Initial commit

--- 03.sql
+++ 03.sql
@@ -126,5 +126,19 @@ student_id	name	trimesters_avg_scores	consistent_improvement
 -- ) AS avg_scores ON students.id = avg_scores.student_id
 -- ORDER BY students.id DESC;
 
+/* Task:
+Each day a plant is growing by upSpeed meters. Each night that plant's height decreases 
+by downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall. 
+We plant the seed at the beginning of a day. We want to know when the height of the plant 
+will reach a certain level.
 
+Example:
+For upSpeed = 100, downSpeed = 10 and desiredHeight = 910, the output should be 10. */
 
+-- SELECT
+--   id,
+--   CASE
+--     WHEN desired_height <= up_speed THEN 1
+--     ELSE CEIL((desired_height * 1.0 - up_speed) / (up_speed - down_speed))::integer + 1
+--   END AS num_days
+-- FROM growing_plant;
